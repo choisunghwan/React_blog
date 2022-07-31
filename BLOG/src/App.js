1688e4f94fa7a2c56e99609d9039978d44c96504
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-07-31 23:17:49 +0900

    리액트 컴포넌트에 대해

--- BLOG/src/App.js
+++ BLOG/src/App.js
@@ -42,8 +42,23 @@ function App() {
         <hr/>
       </div>
 
+      
+
+       <Modal/>
+    
+
+
+
     </div>
   );
 }
-
+function Modal(){
+  return(
+  <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p> 
+        </div>
+  )
+}
 export default App;
