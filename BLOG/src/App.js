376e4cf89062d97211420f44bab89e19b46ab008
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-07-31 23:05:02 +0900

    state 에 대해 알아보기

--- BLOG/src/App.js
+++ BLOG/src/App.js
@@ -5,17 +5,24 @@ import './App.css';
 function App() {
 
 
-  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학','여자 코트 추천']);
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
   let [따봉,따봉변경] = useState(0);
+  let posts = '여자 코트 추천';
 
-  let posts = '강남 고기 맛집';
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천'
+    //글제목변경(['여자 코트 추천','강남 우동 맛집','파이썬 독학'])
+    글제목변경(newArray)
+  }
+
 
   return (
     <div className="App">
       <div className='black-nav'>
         <div>개발 Blog</div>
       </div>
-      <button>  <span onClick={()=>{글제목변경(글제목[3])}}>버튼</span> </button>
+      <button onClick={제목바꾸기}>  버튼 </button>
 
       <div className="list">
         <h3> {글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h3>
