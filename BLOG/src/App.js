5a4889ea2794b4e234f0286de14dee9677749368
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-07-31 22:52:58 +0900

    리액트에서 버튼에 이벤트 리스터(핸들러)장착하는 법

--- BLOG/src/App.js
+++ BLOG/src/App.js
@@ -1,17 +1,40 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학','여자 코트 추천']);
+  let [따봉,따봉변경] = useState(0);
 
   let posts = '강남 고기 맛집';
 
   return (
     <div className="App">
       <div className='black-nav'>
-        <div style={{color:'blue',fontSize:'30px'}}>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <img src={logo}></img>
-      <h4>{posts}</h4>
+      <button>  <span onClick={()=>{글제목변경(글제목[3])}}>버튼</span> </button>
+
+      <div className="list">
+        <h3> {글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h3>
+        <p>7월 31일 발행</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> {글제목[1]} </h3>
+        <p>7월 31일 발행</p>
+        <hr/>
+      </div>
+
+      <div className="list">
+        <h3> {글제목[2]} </h3>
+        <p>7월 31일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
